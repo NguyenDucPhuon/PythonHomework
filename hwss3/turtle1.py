52c4b4701c5d1d93f6b51205447bbfd73f54682e
NguyenDucPhuon <nguyenducphuong2308@gmail.com>
2018-07-29 02:30:41 +0700

    HW SS3

--- hwss3/turtle1.py
+++ hwss3/turtle1.py
@@ -3,13 +3,14 @@ from turtle import *
 speed(0)
 
 colors = ['red', 'blue', 'brown', 'yellow', 'grey']
-angles = [3,4,5,6,7]
+angles = [3, 4, 5, 6, 7]
 
 
 def angle(n):
-    for a in range(n):
+    rotate = 360/n
+    for _ in range(n):
         forward(100)
-        left(360/n)
+        left(rotate)
 
 
 for a, b in zip(colors, angles):
