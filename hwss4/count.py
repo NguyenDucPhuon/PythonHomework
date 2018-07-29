413fd509327fdc31fa77c4a3b0375de239f91ea7
NguyenDucPhuon <nguyenducphuong2308@gmail.com>
2018-07-29 23:14:11 +0700

    HW SS4

---
+++ hwss4/count.py
@@ -0,0 +1,21 @@
+list_1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+list_2 = []
+
+user_string = input("What is your fucking string ????? ")
+
+for _ in user_string:
+    list_2.append(_)
+for letters in list_1:
+    print(letters, list_2.count(letters))
+
+
+
+
+
+
+
+
+
+
+
